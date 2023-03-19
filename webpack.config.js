d5ed0a8792c034c9ee87f5f3e5c43d9b73d0a862
Nikolai Ochirov <dubdubman@ya.ru>
2023-03-19 15:39:31 +0300

    added normal bg images

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 
 const devServer = (isDev) => !isDev ? {} : {
     devServer: {
@@ -53,6 +54,26 @@ module.exports = ({ develop }) => ({
                 type: 'asset'
             }
         ]
-    },
-    ...devServer(develop),
+    }, ...devServer(develop),
+    optimization: {
+        minimizer: [
+            '...',
+            new ImageMinimizerPlugin({
+                minimizer: {
+                    implementation: ImageMinimizerPlugin.imageminMinify,
+                    options: {
+                        // Lossless optimization with custom option
+                        // Feel free to experiment with options for better result for you
+                        plugins: [
+                            ['gifsicle', { interlaced: true }],
+                            ['jpegtran', { progressive: true }],
+                            ['optipng', { optimizationLevel: 5 }],
+                            ['imagemin-webp']
+                        ],
+                    },
+                },
+            }),
+        ],
+    }
+
 });
